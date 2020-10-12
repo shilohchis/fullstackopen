dfba091097f3b844e802ea83a4edaa2eef970256
Shidei Chis <shidei.chis@gmail.com>
2020-10-12 20:40:30 +0700

    Finish 1.7: unicafe step2

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -12,6 +12,10 @@ const App = () => {
     const [neutral, setNeutral] = useState(0);
     const [bad, setBad] = useState(0);
 
+    const goodScore = 1;
+    const neutralScore = 0;
+    const badScore = 1;
+
     const goodClickHandler = () => {
         setGood(good + 1);
     };
@@ -24,15 +28,29 @@ const App = () => {
         setBad(bad + 1);
     };
 
+    const avgScore = () => {
+        const totalScore = (goodScore * good) + (neutralScore * neutral) - (badScore * bad);
+        const totalCount = good + neutral + bad;
+        return totalScore / totalCount || 0;
+    };
+
+    const positivePercentage = () => {
+        return `${( good / (good + neutral + bad) ) * 100 || 0} %`;
+    };
+
     return (
         <div>
             <Header title="give feedback"/>
             <Button text="good" clickHandler={goodClickHandler}/>
             <Button text="neutral" clickHandler={neutralClickHandler}/>
             <Button text="bad" clickHandler={badClickHandler}/>
+            <Header title="statistics"/>
             <Display text="good" value={good}/>
             <Display text="neutral" value={neutral}/>
             <Display text="bad" value={bad}/>
+            <Display text="all" value={good + neutral + bad}/>
+            <Display text="average" value={avgScore()}/>
+            <Display text="positive" value={positivePercentage()}/>
         </div>
     );
 };
