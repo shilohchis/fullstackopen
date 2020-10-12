989b8abc07d377141a2e4b7889363dd188b6ff74
Shidei Chis <shidei.chis@gmail.com>
2020-10-12 20:45:01 +0700

    Finish 1.8: unicafe step3

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -7,26 +7,10 @@ const Button = ({ text, clickHandler }) => <button onClick={clickHandler}>{ text
 
 const Display = ({ text, value }) => <p>{ text } { value }</p>
 
-const App = () => {
-    const [good, setGood] = useState(0);
-    const [neutral, setNeutral] = useState(0);
-    const [bad, setBad] = useState(0);
-
+const Statistics = ({ good, neutral, bad }) => {
     const goodScore = 1;
     const neutralScore = 0;
     const badScore = 1;
-
-    const goodClickHandler = () => {
-        setGood(good + 1);
-    };
-
-    const neutralClickHandler = () => {
-        setNeutral(neutral + 1);
-    };
-
-    const badClickHandler = () => {
-        setBad(bad + 1);
-    };
 
     const avgScore = () => {
         const totalScore = (goodScore * good) + (neutralScore * neutral) - (badScore * bad);
@@ -40,10 +24,6 @@ const App = () => {
 
     return (
         <div>
-            <Header title="give feedback"/>
-            <Button text="good" clickHandler={goodClickHandler}/>
-            <Button text="neutral" clickHandler={neutralClickHandler}/>
-            <Button text="bad" clickHandler={badClickHandler}/>
             <Header title="statistics"/>
             <Display text="good" value={good}/>
             <Display text="neutral" value={neutral}/>
@@ -51,6 +31,34 @@ const App = () => {
             <Display text="all" value={good + neutral + bad}/>
             <Display text="average" value={avgScore()}/>
             <Display text="positive" value={positivePercentage()}/>
+        </div>
+    );
+};
+
+const App = () => {
+    const [good, setGood] = useState(0);
+    const [neutral, setNeutral] = useState(0);
+    const [bad, setBad] = useState(0);
+
+    const goodClickHandler = () => {
+        setGood(good + 1);
+    };
+
+    const neutralClickHandler = () => {
+        setNeutral(neutral + 1);
+    };
+
+    const badClickHandler = () => {
+        setBad(bad + 1);
+    };
+
+    return (
+        <div>
+            <Header title="give feedback"/>
+            <Button text="good" clickHandler={goodClickHandler}/>
+            <Button text="neutral" clickHandler={neutralClickHandler}/>
+            <Button text="bad" clickHandler={badClickHandler}/>
+            <Statistics good={good} neutral={neutral} bad={bad}/>
         </div>
     );
 };
