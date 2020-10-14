a372b0ca8162484e45d08246a8505979f7ab8751
Shidei Chis <shidei.chis@gmail.com>
2020-10-14 23:06:42 +0700

    Finish 2.4: Course information step9

--- part2/courseinfo/src/index.js
+++ part2/courseinfo/src/index.js
@@ -9,7 +9,7 @@ const Total = ({ course }) => {
     }, 0);
 
     return(
-        <p>Total of {sum} exercises</p>
+        <h3>Total of {sum} exercises</h3>
     );
 };
 
@@ -28,29 +28,56 @@ const Course = ({ course }) => {
 };
 
 const App = () => {
-  const course = {
-    id: 1,
-    name: 'Half Stack application development',
-    parts: [
+    const courses = [
         {
-            name: 'Fundamentals of React',
-            exercises: 10,
-            id: 1
+            name: 'Half Stack application development',
+            id: 1,
+            parts: [
+                {
+                    name: 'Fundamentals of React',
+                    exercises: 10,
+                    id: 1
+                },
+                {
+                    name: 'Using props to pass data',
+                    exercises: 7,
+                    id: 2
+                },
+                {
+                    name: 'State of a component',
+                    exercises: 14,
+                    id: 3
+                },
+                {
+                    name: 'Redux',
+                    exercises: 11,
+                    id: 4
+                }
+            ]
         },
         {
-            name: 'Using props to pass data',
-            exercises: 7,
-            id: 2
-        },
-        {
-            name: 'State of a component',
-            exercises: 14,
-            id: 3
+            name: 'Node.js',
+            id: 2,
+            parts: [
+                {
+                    name: 'Routing',
+                    exercises: 3,
+                    id: 1
+                },
+                {
+                    name: 'Middlewares',
+                    exercises: 7,
+                    id: 2
+                }
+            ]
         }
-        ]
-    }
+    ]
 
-    return <Course course={course} />
+    return (
+        <>
+            { courses.map(course => <Course key={course.id} course={course} />) }
+        </>
+    );
 }
 
 ReactDOM.render(<App />, document.getElementById('root'))
