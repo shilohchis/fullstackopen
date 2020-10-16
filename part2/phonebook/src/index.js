988bb1d8a8cd1a9e05f0d1ae1bcebfa7676dcf31
Shidei Chis <shidei.chis@gmail.com>
2020-10-16 22:54:48 +0700

    Finish 2.8: The Phonebook Step3

--- part2/phonebook/src/index.js
+++ part2/phonebook/src/index.js
@@ -1,40 +1,58 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
+import FormBasic from './sections/FormBasic';
+import FlatList from './components/FlatList';
+import HeaderType from './components/HeaderType';
 
 const App = () => {
     const [ newName, setNewName ] = useState('');
+    const [ newPhone, setNewPhone ] = useState('');
     const [ counter, setCounter ] = useState(1);
     const [ persons, setPersons ] = useState([
-        { name: 'Arto Hellas', id: counter }
+        { name: 'Arto Hellas', phone: '040-1829102', id: counter }
     ]);
 
-    const changeInputName = e => setNewName(e.target.value);
+    const changeInputVal = ({ target: { value } }, type = null) => {
+        switch (type) {
+            case 'name':
+                setNewName(value);
+                break;
+            default:
+                setNewPhone(value);
+        }
+    };
+
+    const resetInput = () => {
+        setNewName('');
+        setNewPhone('');
+    };
 
     const addName = e => {
         e.preventDefault();
         const find = persons.find( obj => obj.name === newName );
         if(!find) {
-            setPersons([ ...persons, { name: newName, id: counter + 1 } ]);
+            setPersons([ ...persons, { name: newName, phone: newPhone, id: counter + 1 } ]);
             setCounter(counter + 1);
-            setNewName('');
+            resetInput();
         } else {
             alert(`${newName} is already added to phonebook`);
         }
     };
 
+    const dataForm = {
+        button: { type: "submit", label: "add", submitHandler: addName },
+        inputs: [
+            { label: 'name', value: newName, valueHandler: changeInputVal },
+            { label: 'phone', value: newPhone, valueHandler: changeInputVal },
+        ]
+    };
+
     return (
         <div>
-            <h2>Phonebook</h2>
-            <form onSubmit={addName}>
-                <div>
-                    name: <input value={newName} onChange={changeInputName}/>
-                </div>
-                <div>
-                    <button type="submit">add</button>
-                </div>
-            </form>
-            <h2>Numbers</h2>
-            { persons.map(item => <p key={item.id}>{ item.name }</p>) }
+            <HeaderType number={2} text="Phonebook"/>
+            <FormBasic data={dataForm}/>
+            <HeaderType number={2} text="Numbers"/>
+            <FlatList datas={persons} showKeys={['name', 'phone']}/>
         </div>
     );
 };
