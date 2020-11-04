cd20e69c82be9a5b29e978ccbe5aadcf2a200907
Shidei Chis <shidei.chis@gmail.com>
2020-11-04 22:11:07 +0700

    Finish 3.2: Phonebook backend step2

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -33,6 +33,13 @@ app.get('/api/persons', (req, resp) => {
     return resp.status(200).json(books);
 });
 
+app.get('/info', (req, resp) => {
+    return resp.send(`
+        <p>Phonebook has info for ${books.length} people</p>
+        <p>${new Date()}</p>
+    `);
+});
+
 const PORT = 3001;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
